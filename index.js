a8d17b91a1c7bafff5b61f85a45c96483481cf37
Ryan Nourbaran <ryan.nourbaran@hotmail.com>
2019-01-15 11:01:13 -0500

    qr scanner working

---
+++ index.js
@@ -0,0 +1,65 @@
+/** @format */
+
+"use strict";
+
+import React, { Component } from "react";
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking
+} from "react-native";
+
+import App from "./App";
+import { name as appName } from "./app.json";
+import QRCodeScanner from "react-native-qrcode-scanner";
+
+class ScanScreen extends Component {
+  onSuccess(e) {
+    Linking.openURL(e.data).catch(err =>
+      console.error("An error occured", err)
+    );
+  }
+
+  render() {
+    return (
+      <QRCodeScanner
+        onRead={this.onSuccess.bind(this)}
+        topContent={
+          <Text style={styles.centerText}>
+            <Text style={styles.textBold}>DeltaHacks</Text>
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 20,
+    color: "#777"
+  },
+  textBold: {
+    fontWeight: "500",
+    color: "#000"
+  },
+  buttonText: {
+    fontSize: 21,
+    color: "rgb(0,122,255)"
+  },
+  buttonTouchable: {
+    padding: 10
+  }
+});
+
+AppRegistry.registerComponent(appName, () => ScanScreen);
