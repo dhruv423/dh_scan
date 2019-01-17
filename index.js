bb33500b34bbbf4df3e13d0772abc811ee638510
Ryan Nourbaran <ryan.nourbaran@hotmail.com>
2019-01-16 23:46:51 -0500

    disabled warnings

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ import App from "./App";
 import { name as appName } from "./app.json";
 import QRCodeScanner from "./src/screens/QRCodeScanner";
 
+console.disableYellowBox = true; //get rid of pesky warnings
 AppRegistry.registerComponent(appName, () => App);
